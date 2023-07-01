35054735a4e6fec160086d74db9e6df3d23b9c1d
auresolll <usool.203@gmail.com>
2023-07-01 16:52:49 +0700

    [DEPLOY]: Fix  declared but its value is never read

--- cpanel/src/Pages/Home/Home.tsx
+++ cpanel/src/Pages/Home/Home.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from 'react';
-import styles from './Home.module.scss';
+import { FC } from "react";
+import styles from "./Home.module.scss";
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = () => (
-  <div className={styles.Home}>
-    Home Component
-  </div>
+  <div className={styles.Home}>Home Component</div>
 );
 
 export default Home;
