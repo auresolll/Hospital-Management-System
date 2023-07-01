975809f923360684d2731c9b35e49d0427aa1460
auresolll <usool.203@gmail.com>
2023-07-01 15:14:31 +0700

    [SERVICES]: authentication service

---
+++ cpanel/src/Services/authentication/authentication.interface.ts
@@ -0,0 +1,6 @@
+import { typeRegister, typeSign } from "./authentication.type";
+
+export interface IAuthentication {
+  signIn(user: typeSign): void;
+  register(user: typeRegister): void;
+}
---
+++ cpanel/src/Services/authentication/authentication.type.ts
@@ -0,0 +1,12 @@
+export type typeSign = {
+  username: string;
+  password: string;
+};
+
+export type typeRegister = {
+  name: string;
+  country: string;
+  phone: string;
+  gender: string;
+  password: string;
+};
